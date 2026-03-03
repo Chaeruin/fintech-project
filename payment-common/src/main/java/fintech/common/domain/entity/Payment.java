84fd0cc790730e583cab73c05888a700998c0e7c
Chaeruin <cms3604@naver.com>
2026-03-04 08:43:30 +0900

    feat: common-Payment entity 구성

---
+++ payment-common/src/main/java/fintech/common/domain/entity/Payment.java
@@ -0,0 +1,49 @@
+package fintech.common.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 가맹점 주문번호
+    @Column(nullable = false, unique = true)
+    private String orderId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    // PG사 거래 키
+    private String transactionKey;
+
+    // Spring 미적용 -> @CreatedDate 적용 불가
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
