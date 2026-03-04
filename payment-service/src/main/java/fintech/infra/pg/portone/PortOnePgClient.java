6d7dc5862362b85155da91170d3ddf541a8b7b5f
Chaeruin <cms3604@naver.com>
2026-03-04 11:25:01 +0900

    feat: portonePaymentWebClient 빈을 주입받아 사용하는 구현체 - PortOnePgClient 구현

---
+++ payment-service/src/main/java/fintech/infra/pg/portone/PortOnePgClient.java
@@ -0,0 +1,33 @@
+package fintech.infra.pg.portone;
+
+
+import fintech.infra.pg.PgClient;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class PortOnePgClient implements PgClient {
+
+    @Qualifier("portonePaymentWebClient")
+    private final WebClient webClient;
+
+    @Override
+    public void confirm(String paymentKey, String orderId, BigDecimal amount) {
+        // 포트원 스펙에 맞는 엔드포인트와 데이터 구조
+        webClient.post()
+                .uri("/payments/confirm")
+                .bodyValue(new PortOneConfirmRequest(paymentKey, amount))
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+    }
+
+    @Override
+    public String getPgType() { return "PORTONE"; }
+
+    private record PortOneConfirmRequest(String imp_uid, BigDecimal amount) {}
+}
