00c03996263b7b1b21b66887ef284d264127e825
Chaeruin <cms3604@naver.com>
2026-03-04 12:37:26 +0900

    feat: PaymentApi 컨트롤러 작성

---
+++ payment-service/src/main/java/fintech/api/PaymentApi.java
@@ -0,0 +1,26 @@
+package fintech.api;
+
+import fintech.application.PaymentService;
+import fintech.application.dto.PaymentConfirmCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/payments")
+@RequiredArgsConstructor
+public class PaymentApi {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/confirm")
+    public ResponseEntity<String> confirmPayment(
+            @RequestBody PaymentConfirmCommand command
+    ) {
+        paymentService.confirm(command);
+        return ResponseEntity.ok("결제가 성공적으로 완료되었습니다.");
+    }
+}
