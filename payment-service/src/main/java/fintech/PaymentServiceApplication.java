0948ac4e5e419f699a8304b880ed7764b5504709
Chaeruin <cms3604@naver.com>
2026-03-04 11:35:36 +0900

    fix: payment-service Spring Boot Application 코드에서 repostiory 인식 범위 설정

--- payment-service/src/main/java/fintech/PaymentServiceApplication.java
+++ payment-service/src/main/java/fintech/PaymentServiceApplication.java
@@ -4,10 +4,12 @@ package fintech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @EntityScan(basePackages = "fintech.payment.common.domain")
+@EnableJpaRepositories(basePackages = "fintech.payment.service.infrastructure.persistence")
 public class PaymentServiceApplication { //
 
     public static void main(String[] args) {
