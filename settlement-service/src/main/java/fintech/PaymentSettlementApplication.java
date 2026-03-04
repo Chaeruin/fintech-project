1af151a55715e07757fa760ba57c92fcbf14d843
Chaeruin <cms3604@naver.com>
2026-03-04 13:07:28 +0900

    fix: settlement-service 에서 common 모듈 엔티티 인식할 수 있도록 설정

--- settlement-service/src/main/java/fintech/PaymentSettlementApplication.java
+++ settlement-service/src/main/java/fintech/PaymentSettlementApplication.java
@@ -3,8 +3,12 @@ package fintech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "fintech.payment.common.domain")
+@EnableJpaRepositories(basePackages = "fintech.settlement.service.infrastructure.persistence")
 public class PaymentSettlementApplication { //
 
     public static void main(String[] args) {
