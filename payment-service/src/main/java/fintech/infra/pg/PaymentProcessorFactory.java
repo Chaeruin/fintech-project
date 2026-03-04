048eb378d1c7c8ea78a3aaf8e3f82fde8bfd8f90
Chaeruin <cms3604@naver.com>
2026-03-04 15:37:24 +0900

    feat: PaymentProcessorFactory 에서 pgType 문자열에 따라 프로세서 서칭

---
+++ payment-service/src/main/java/fintech/infra/pg/PaymentProcessorFactory.java
@@ -0,0 +1,27 @@
+package fintech.infra.pg;
+
+import fintech.common.global.exception.CustomException;
+import fintech.common.global.exception.ErrorCode;
+import fintech.domain.service.PaymentProcessor;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentProcessorFactory {
+
+    private final Map<String, PaymentProcessor> processors = new ConcurrentHashMap<>();
+
+    public PaymentProcessorFactory(PortOnePaymentProcessor portOne, TossPaymentProcessor toss) {
+        processors.put("PORTONE", portOne);
+        processors.put("TOSS", toss);
+    }
+
+    public PaymentProcessor getProcessor(String pgType) {
+        PaymentProcessor processor = processors.get(pgType.toUpperCase());
+        if (processor == null) {
+            throw new CustomException(ErrorCode.EXTERNAL_PG_ERROR);
+        }
+        return processor;
+    }
+}
