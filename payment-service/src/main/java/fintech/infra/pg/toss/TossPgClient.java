42cf463d84b32e372fc2a66a99dc3693c28d0e4d
Chaeruin <cms3604@naver.com>
2026-03-04 11:22:20 +0900

    feat: tossPaymentWebClient 빈을 주입아 사용하는 구현체 - TossPgClient 구현

---
+++ payment-service/src/main/java/fintech/infra/pg/toss/TossPgClient.java
@@ -0,0 +1,34 @@
+package fintech.infra.pg.toss;
+
+
+import fintech.infra.pg.PgClient;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class TossPgClient implements PgClient {
+
+    @Qualifier("tossPaymentWebClient")
+    private final WebClient webClient;
+
+    @Override
+    public void confirm(String paymentKey, String orderId, BigDecimal amount) {
+        webClient.post()
+                .uri("/v1/payments/confirm")
+                .bodyValue(new TossConfirmRequest(paymentKey, orderId, amount))
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+    }
+
+    @Override
+    public String getPgType() {
+        return "TOSS";
+    }
+
+    private record TossConfirmRequest(String paymentKey, String orderId, BigDecimal amount) {}
+}
